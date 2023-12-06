0d07e2c6c9262b06dd31c6f271d291375d5fd5a6
Ebahena2 <89425354+Ebahena2@users.noreply.github.com>
2023-12-05 23:10:15 -0600

    adding validation on fields for js file

--- js/screen.js
+++ js/screen.js
@@ -13,3 +13,20 @@ jQuery(function($){
   
   });
   });
+  
+function validation(){
+  var first-name = document.getElementById("first name").value;
+  var email = document.getElementById("email").value;
+  var last-name = document.getElementById("last name").value;
+  var country = document.getElementById("country").value;
+  var city = document.getElementById("city").value;
+  var state = document.getElementById("State").value;
+  var postal = document.getElementById("postal code").value;
+  var institution = document.getElementById("Undergraduate Instition").value;
+  var grad = document.getElementById("graduation year").value;
+  if(first-name === "" || email === "" || last-name === "" || country === "" || city === "" || state === "" || postal === "" || institution === "" || grad === ""){
+    alert("Fill out the field");
+    return false;
+    }
+    return true;
+}
