d9578a51f447f2aad718869452319fd38644f449
Ebahena2 <89425354+Ebahena2@users.noreply.github.com>
2023-12-05 23:20:25 -0600

    added things for js in index

--- js/screen.js
+++ js/screen.js
@@ -1,19 +1,4 @@
-jQuery(function($){
-  $('html').removeClass('nojs');
-  $('html').addClass('hasjs');
-  
-});
- 
- 
-   $(document).ready(function(){
-    $("#submit").click(function(){
-      if(confirm("Are you ready to submit?")){
-      alert("Application has been sent");}
-      else alert("Application has not worked");
-  
-  });
-  });
-  
+
 function validation(){
   var first-name = document.getElementById("first name").value;
   var email = document.getElementById("email").value;
