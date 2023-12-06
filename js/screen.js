26c3abf5eee1092e17ee2885c916ae3f0730f052
Ebahena2 <89425354+Ebahena2@users.noreply.github.com>
2023-12-05 23:36:38 -0600

    added required parameteres to  form

--- js/screen.js
+++ js/screen.js
@@ -1,17 +1,15 @@
-
-function validation(){
-  var first-name = document.getElementById("first name").value;
-  var email = document.getElementById("email").value;
-  var last-name = document.getElementById("last name").value;
-  var country = document.getElementById("country").value;
-  var city = document.getElementById("city").value;
-  var state = document.getElementById("State").value;
-  var postal = document.getElementById("postal code").value;
-  var institution = document.getElementById("Undergraduate Instition").value;
-  var grad = document.getElementById("graduation year").value;
-  if(first-name === "" || email === "" || last-name === "" || country === "" || city === "" || state === "" || postal === "" || institution === "" || grad === ""){
-    alert("Fill out the field");
-    return false;
-    }
-    return true;
-}
+jQuery(function($){
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+  
+});
+ 
+ 
+   $(document).ready(function(){
+    $("#submit").click(function(){
+      if(confirm("Are you ready to submit?")){
+      alert("Application has been sent");}
+      else alert("Application has not worked");
+  
+  });
+  });
